92d33f8e03c75318c67060a671cd5cf2c9f9a0b9
anasramzyy <132026783+anasramzyy@users.noreply.github.com>
2024-04-23 08:03:49 +0200

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
   return await mongoose
-    .connect(process.env.CONNECTION_STRING)
+    .connect(process.env.CONNECTION_STRING, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true })
     .then(() => console.log("Db Connected Successfully"))
     .catch((error) => {
       console.log("Error: ", error)
