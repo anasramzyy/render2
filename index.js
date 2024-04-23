748e17aef583c142fe22e3aa16919fb5dc089b5f
anasramzyy <132026783+anasramzyy@users.noreply.github.com>
2024-04-23 08:05:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-dotenv.config()
+dotenv.config({path: '.env'})
 import { connectDB } from "./db/connection.js"
 import { startApp } from "./src/app.router.js"
 import { checkDataBase, sendEmails } from "./src/utils/cronjobs.js"
